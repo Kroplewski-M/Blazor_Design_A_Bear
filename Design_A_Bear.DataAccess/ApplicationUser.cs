bdb82df69e61f5ce0a4a3127fef38eb38446414b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-01-04 16:38:13 +0000

    added custom user propeties

--- Design_A_Bear.DataAccess/ApplicationUser.cs
+++ Design_A_Bear.DataAccess/ApplicationUser.cs
@@ -2,9 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Design_A_Bear.DataAccess
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 
 }
