660314b3bebb67a762260cd624e06ba0feba87b9
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-01-17 18:04:43 +0000

    started form to create item

--- Design_A_Bear.Models/Item.cs
+++ Design_A_Bear.Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,19 @@ namespace Design_A_Bear.Models
     public class Item
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
+        [Required]
+        [Range(1, 1000)]
         public float Price { get; set; }
+        [Required]
+
         public string Category { get; set; }
+        [Required]
+
         public string ImgBase64 { get; set; }
 
     }
